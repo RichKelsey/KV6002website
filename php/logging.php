121ce7030e3ce8e6e389bf652b04aa266d4d1528
Sheldon Lee <sheldon@sheldonlee.com>
2023-02-14 23:06:06 +0000

    Add logging functions for php.
    Logging writes to php.log in the project root if the DEBUG constant is
    not defined.
    
    - Added log_print() function.
    
    - Added log_error() function that calles log_print, but prepends the
      message, indicating that it is an error message.

---
+++ php/logging.php
@@ -0,0 +1,29 @@
+<?php
+define("DEBUG", null);
+define("LOG_FILE", __DIR__ . "/../php.log");
+define("PREFIX_ERROR", "Error: ");
+
+function log_print($string)
+{
+	if(defined("DEBUG")) {
+		echo $string . "<br>";
+		return;
+	}
+
+	$handle = fopen(LOG_FILE, "a");
+	if (!$handle) { 
+		echo "Unable to open file.<br>";
+		return;
+	}
+	
+	if (!fwrite($handle, $string . "\n")) {
+		echo "Unable to write to file.<br>";
+	}
+	fclose($handle);
+}
+
+function log_error($string)
+{
+	log_print(PREFIX_ERROR . $string);
+}
+?>
