b212f8825066220bc94d459c61b99e32b2962e7b
marinasot22 <116646622+marinasot22@users.noreply.github.com>
2023-03-24 22:46:15 +0000

    Update userprofile.php
    my updated version

--- pages/userprofile.php
+++ pages/userprofile.php
@@ -14,96 +14,157 @@ require_once("../php/db_connection.php");
 if (!$db) die("Can't connect to database");
 
 
-//write entered values into variables to later display
-require_once "../php/credentials.php";
-
 $directory = "../img/avatars/";
 $files = array();
 
-// Check if the form has been submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Collect user input
-    $Name = $_POST['name'];
-    $Bio = $_POST['bio'];
+// Get the maximum ID from the users table
+$sql = "SELECT MAX(id) as max_id FROM users";
+$result = mysqli_query($db, $sql);
 
-    echo"<br><label for='avatar[]'>Select an avatar:<br><br>";
+// Check if query was successful
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $PartcipantID = $row['max_id'] + 1; // Create a new ID by adding 1 to the maximum ID
+} else {
+    $PartcipantID = 1; // If the table is empty, start the IDs from 1
+}
 
- 				//display all available avatars
- 				foreach (scandir($directory) as $file) 
+// Get user data from the form
+$username = $_POST['username'];
+$bio = $_POST['bio'];
+
+
+//select Avatar
+  echo"<br><label for='avatar[]'>Select an avatar:<br><br>";
+
+ 	//display all available avatars
+ 		foreach (scandir($directory) as $file) 
+ 			{
+ 			 if ($file !== '.' && $file !== '..') 
  				{
- 					if ($file !== '.' && $file !== '..') 
- 					{
- 						$files[] = $file;
+ 					$files[] = $file;
 
- 						echo"<img src='../img/avatars/$file'" . "width='40' height='40' />";
-					}
-				}
+ 					echo"<img src='../img/avatars/$file'" . "width='40' height='40' />";
+                        
+            echo"<div class='avatarcontainer'>";
+                                 
+              echo"<label for='avatar'> <img src='../img/avatars/$file'/> </label>";
+          }
+            else
+            {
 
+              echo"<input type='checkbox' id='avatar' name='avatar[]' value='{$file}' onclick='AvatarSelection(\"$file\")'>";
+                echo"</div>";
 
-    // Validate input
-    if (empty($Name) || empty($Bio)) {
-        // Error: All fields are required
-        echo "All fields are required";
-    } elseif (strlen($Bio) < 400) {
+                }
+                                 
+              $tmp +=1;
+                                 
+                                
+               }
+
+// Validate input
+  if (empty($username) || empty($bio)) {
+  // Error: All fields are required
+     echo "All fields are required";
+    } elseif (strlen($bio) > 400) {
         // Error: Bio must be at least 400 characters
         echo "Bio must be at least 400 characters";
-    } else {
-        // Hash Bio
-        $hashed_Bio = bio_hash($Bio, BIO_DEFAULT);
+    } 
 
-        // Connect to database
-        $conn = new mysqli('localhost', 'username', 'bio', 'ProfilePic');
+// Check connection
+  if ($db->failed) {
+    die("Connection failed: " . $db->failed);
+    }
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        // Insert user into database
-        $sql = "INSERT INTO users (name , bio) VALUES ('$Name', '$hashed_Bio')";
-        if ($conn->query($sql) === TRUE) {
-            echo "User created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $db->error;
+// Insert user into database
+$conn = "INSERT INTO Participants (PartcipantID, username, bio) VALUES ('$PartcipantID', '$username', '$bio')";
+  if ($db->query($sql) === TRUE) {
+    echo "User created successfully";
+     } else {
+        echo "Error: " . $sql . "<br>" . $db->error;
         }
 
     
-    }
-}
-
-// get user information
-//$user_id = $conn->query('SELECT ParticipandID FROM Participant');// replace with the actual user ID
-//$user_name = $conn->query('SELECT Name FROM Participant'); // replace with the actual user name
 
 // randomly allocate user to a group
-//$group = $conn->query('SELECT GroupID FROM Group');
+$group = $db->query('SELECT GroupID FROM Group');
 
-// insert user and group information into database
-//$sql = "INSERT INTO Participant (ParticipantID, GroupID, Name)
-//        VALUES ('$user_id', '$user_name', '$group')";
 
-//if (mysqli_query($conn, $sql)) {
-//    echo "User allocated to group $group";
-//} else {
-//    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-//}
+if (mysqli_query($db, $sql)) {
+    echo "User allocated to group $group";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+}
+
+// insert user's group into database
+$sql = "INSERT INTO Participant (GroupID)
+        VALUES ('$PartcipantID', '$group')";
+
+//write entered values into variables to later display
+require_once "../php/db_connection.php";
 
 ?>
-<!-- HTML form -->
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <label for="name">Name:</label>
-  <input type="text" name="name" id="name" value="<?php echo $name;?>">
-  <span class="error"><?php echo $nameErr;?></span><br><br>
 
-  <label for="email">Email:</label>
-  <input type="email" name="email" id="email" value="<?php echo $email;?>">
-  <span class="error"><?php echo $emailErr;?></span><br><br>
+<script>
+//Allow only one checkbox to be selected
 
-  <label for="password">Password:</label>
-  <input type="password" name="password" id="password">
-  <span class="error"><?php echo $passwordErr;?></span><br><br>
+function selectOnlyOne(checkbox) 
+			{
+				var checkboxes = document.getElementsByName('avatar[]');
+				checkboxes.forEach((item) => 
+				{
+					if (item !== checkbox) item.checked = false;
+				});
+			}
 
-  <input type="submit" name="submit" value="Sign Up">
+ //Check if an avatar has been selected
+
+			function validateForm()
+			{
+				var checkboxes = document.getElementsByName("avatar[]");
+				var checked = false;
+				for (var i = 0; i < checkboxes.length; i++)
+				{
+					if (checkboxes[i].checked) 
+					{
+						checked = true;
+						break;
+					}
+				}
+				if (!checked)
+				{
+					alert("Please select an Avatar to continue.");
+					return false;
+				}
+			}
+			
+      </script>
+
+<form>
+
+  <fieldset disabled>
+    <legend>Create Profile</legend>
+    <div class="col-auto">
+    <label for="staticUsername" class="visually-hidden">Username</label>
+    <input type="text" readonly class="form-control-plaintext" id="staticusername" value="username">
+  </div>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Bio</label>
+      <input type="text" id="disabledTextInput" class="form-control" placeholder="Bio">
+    </div>
+    <div class="mb-3">
+      <label for="disabledSelect" class="form-label">Select Avatar</label>
+      <select id="disabledSelect" class="form-select">
+        <option>Avatars</option>
+      </select>
+    </div>
+    
+    <button onclick="location.href='newsfeed.php'">Complete your profile </button>
+    <button onclick="location.href='login.php'">Return to Login Page</button>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </fieldset>
+
 </form>
 
 </body>
